7bc4d20c8ac1652b70f206d9572ccd393a2979af
Gergo Gacs <gacsgergo@gmail.com>
2025-02-19 20:51:14 +0800

    feat(macos): add music to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Slack.app"
