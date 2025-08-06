bbee25d68374c78a1b2b8390bdf9d479fdee3730
Gergo Gacs <gacsgergo@gmail.com>
2025-08-06 10:25:31 +0200

    cleanup: remove unused sw, ignore junk

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/System/Applications/Music.app"
-dockutil --no-restart --add "/System/Applications/Mail.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/zoom.us.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Affinity Photo 2.app"
-dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
+dockutil --no-restart --add "/Applications/Affinity Photo 2.app"
 dockutil --no-restart --add "/System/Applications/System Settings.app"
 
 killall Dock
